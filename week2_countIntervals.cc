241081a7af7b5fa337910b5cd98b03897e9fc6d5
sinnergarden <987020311@qq.com>
2017-07-09 17:29:45 +0800

    revise quicksort and quick find

--- week2_countIntervals.cc
+++ week2_countIntervals.cc
@@ -6,30 +6,30 @@
 #include<cstdlib>
 using namespace std;
 
-//vector<int> merge(vector<int>, vector<int>);
-inline bool Less(int,int);
+inline bool isLess(int,int);
 void init(int);
-void read_input();
-void print_result(const vector<int> &);
+void readInput();
+void printResult(const vector<int> &);
+
 void merge(vector<int>&, int, int, int, int);
-void sort_and_count(vector<int> &);
-void mergesort(vector<int>&, int, int);
+void sortAndCount(vector<int> &);
+void mergeSort(vector<int>&, int, int);
 
 vector<int> test_array;
 int intervals = 0;
 
-int  main(){
-  //read_input();
+int main(){
+  //readInput();
   init(20);
-  print_result(test_array);
-  sort_and_count(test_array);
-  //mergesort(test_array);
-  print_result(test_array);
+  printResult(test_array);
+  sortAndCount(test_array);
+  //mergeSort(test_array);
+  printResult(test_array);
   cout <<"The number of intervals : "<<intervals <<endl;
 }
 
 
-inline bool Less(int a, int b){
+inline bool isLess(int a, int b){
   return a<b ? 1 : 0;
 }
 
@@ -41,13 +41,13 @@ void init(int length){
   }
 }
 
-void read_input(){
+void readInput(){
   int temp;
   test_array.clear();
   while(cin>>temp) test_array.push_back(temp); 
 }
 
-void print_result(const vector<int> &test_array){
+void printResult(const vector<int> &test_array){
   cout << " The current array is :" << endl;
   for(int i = 0; i < test_array.size(); i++) {
     cout<< test_array[i] << ' ';
@@ -55,15 +55,15 @@ void print_result(const vector<int> &test_array){
   cout << endl;
 }
 
-void sort_and_count(vector<int> &array){
-  mergesort(array, 0, array.size()-1);
+void sortAndCount(vector<int> &array){
+  mergeSort(array, 0, array.size()-1);
 }
 
 void merge(vector<int>&array, int begin1, int end1, int begin2, int end2){
   int i = begin1, j = begin2;
   vector<int> temp;
   while(i <= end1 || j <= end2){
-    if (Less(array[i],array[j]) && i<=end1 && j<=end2){
+    if (isLess(array[i],array[j]) && i<=end1 && j<=end2){
       temp.push_back(array[i]);
       i++;
     } else if (i<=end1 && j<=end2){
@@ -83,9 +83,9 @@ void merge(vector<int>&array, int begin1, int end1, int begin2, int end2){
   }
 }
 
-void mergesort(vector<int> &array, int begin, int end){
+void mergeSort(vector<int> &array, int begin, int end){
   int mid = (begin+end)/2;
-  if (mid > begin) mergesort(array, begin, mid);
-  if (end > mid+1) mergesort(array, mid+1, end);
+  if (mid > begin) mergeSort(array, begin, mid);
+  if (end > mid+1) mergeSort(array, mid+1, end);
   merge(array, begin, mid, mid+1, end);
 }
