eb2ded70665fc83bc00393f92f2a716179f59722
sinnergarden <987020311@qq.com>
2017-07-10 23:55:19 +0800

    strong connected componts

--- week5_SCC.cc
+++ week5_SCC.cc
@@ -5,11 +5,28 @@ using namespace std;
 class DiGraph{
 public:
   DiGraph();
+  DiGraph(int v) {
+    this.V = v;
+    this.E = 0;
+    adj.resize(v+1);
+  }
   int getV() {return V;}
   int getE() {return E;}
-  void addEdge(int v, int w) {adj[v].push_back(w);}
+  void addEdge(int v, int w) {
+    adj[v].push_back(w);
+    E++;
+  }
   vector<int> adj(int v) {return adj[v];}
-  void reverse();
+  void reverse() {
+    vector<vector<int>> aux(this.V+1);
+    for(int v = 1; v <= this.V; v++)
+      for(int w:adj[v])
+	aux[w].push_back(v);
+    for(int v = 1; v <= this.V; v++)
+      for(int w:adj[v])
+	adj[w] = aux[w];
+  }
+  
 private:
   int V;
   int E;
@@ -20,19 +37,43 @@ private:
 class SCC{
 public:
   SSC();
-  void dfs(DiGraph G, int v){
-    marked[v]=true;
+  void dfs1(DiGraph G, int v){
+    marked[v] = true;
+    for(int w: G.adj[v]){
+      if(!marked[w]){
+	dfs(G, w);
+	//reverse post order
+      }
+    }
+  }
+  void dfs2(DiGraph G, int v){
+    marked[v] = true;
     for(int w: G.adj[v]){
       if(!marked[w]){
 	dfs(G, w);
       }
     }
   }
-  void ssc();
+  void ssc(DiGraph G){
+    //init marked
+    for(int v = 1; v <= G.getV(); v++)
+      dfs1(G.reverse(), v);
+    //init marked
+    for(int v: reversePostOrder)
+      count ++;
+      dfs2(G, v);
+  }
 private:
+  int count;
   vector<bool> marked;
+  vector<int> reversePostOrder;
+  vector<int> id;
 };
 
 int main(){
-  
+  //read from file and init Directed Graph G
+
+  //calculate SCC
+
+  //calculate first 5 SCC size
 }
