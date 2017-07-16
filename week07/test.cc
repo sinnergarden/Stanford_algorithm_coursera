58ae7edf155d569b2af59c321c1b0ebfd0e307bc
sinnergarden <987020311@qq.com>
2017-07-16 15:06:58 +0800

    complete week 7

--- week07/test.cc
+++ week07/test.cc
@@ -6,32 +6,47 @@ using namespace std;
 int main(){
   int count = 0;
   double sum = 0;
-  int curMedian;
+  double curMedian = 0;
   int temp;
   ifstream testFile;
   pqMin min;
   pqMax max;
   
-  testFile.open("SmallInput.txt");
-
+  //testFile.open("SmallInput.txt");
+  testFile.open("Median.txt");
+  
   while(testFile >> temp){
-    cout<<"round: "<< count+1 <<endl;
-    if (min.count() >= max.count()) {
-      max.put(temp);
-      count ++;
-      sum += min.getMin();
-      cout<<"median is: "<< min.getMin() <<endl<<endl; 
-    }
-    else {
-      min.put(temp);
-      count ++;
-      sum += (min.getMin()+max.getMax())/2.0;
-      cout<<"median is: "<< (min.getMin()+max.getMax())/2.0 <<endl<<endl; 
-    }
+
+    if (temp > curMedian) min.put(temp);
+    else max.put(temp);
+    
+    count ++;
+    //cout<<"round: "<< count <<endl;
+    
+    while(min.count() > max.count()) //min + 1 = max or min == max
+      max.put(min.delMin());
+
+    while(max.count() > min.count() + 1) 
+      min.put(max.delMax());
+
+    if (min.count() + 1 == max.count())
+      curMedian = max.readMax();
+      
+    if (min.count() == max.count())
+      curMedian = (min.readMin() + max.readMax())/2.0;
+
+    //cout<<"left Max :"<<max.count()<<endl;
+    //cout<<"right Min :"<<min.count()<<endl;
+    //max.print();
+    //min.print();
+    //cout<<"Median is: "<< curMedian << endl;
+    sum += curMedian;
+
   }
 
   testFile.close();
-  
+
+  cout<<endl;
   cout<<"count result: "<< count<<endl;
   cout<<"sum result: "<<sum<<endl;
   cout<<"average median result: " << sum/count << endl;
--- week07/week7_median.hh
+++ week07/week7_median.hh
@@ -7,8 +7,12 @@ public:
   pqMin();
   void put(int);
   void swim(int);
-  int getMin();
+  void sink(int);
+  int delMin();
+  int readMin();
   int count();
+  void print();
+  bool isEmpty();
 private:
   vector<int> pqArray;
 };
@@ -18,8 +22,12 @@ public:
   pqMax();
   void put(int);
   void swim(int);
-  int getMax();
+  void sink(int);
+  int delMax();
+  int readMax();
   int count();
+  void print();
+  bool isEmpty();
 private:
   vector<int> pqArray;
 };
@@ -27,7 +35,7 @@ private:
 pqMin::pqMin() { pqArray.push_back(0); }
 
 void
-pqMin::put(int ele) {
+pqMin::put(int ele) { 
   pqArray.push_back(ele);
   swim(pqArray.size()-1);
 }
@@ -36,14 +44,48 @@ void
 pqMin::swim(int idx) {
   while(idx/2 > 0 && pqArray[idx] < pqArray[idx/2]) {
     swap(pqArray[idx], pqArray[idx/2]);
+    idx /= 2;
+  }
+}
+
+void
+pqMin::sink(int idx) {
+  while(2*idx <= pqArray.size()-1){
+    int j = 2*idx;
+    if(j<pqArray.size()-1 && pqArray[j] > pqArray[j+1]) j++;
+    if(pqArray[idx] < pqArray[j]) break;
+    swap(pqArray[idx], pqArray[j]);
+    idx = j;
   }
 }
 
 int
-pqMin::getMin() { return pqArray[1]; }
+pqMin::readMin() { return pqArray[1]; }
+
+int
+pqMin::delMin() {
+  int min = pqArray[1];
+  swap(pqArray[1], pqArray[pqArray.size()-1]);
+  pqArray.pop_back();
+  sink(1);
+  return min;
+}
 
 int
 pqMin::count() { return pqArray.size()-1; }
+
+void
+pqMin::print() {
+  cout<<"right pq is :";
+  for (int i = 1; i < pqArray.size(); i++)
+    cout<< pqArray[i] <<' ';
+  cout<<endl;
+}
+
+bool
+pqMin::isEmpty() {
+  return pqArray.size() == 1;
+}
 
 pqMax::pqMax() { pqArray.push_back(0); }
 
@@ -57,16 +99,45 @@ void
 pqMax::swim(int idx) {
   while(idx/2 > 0 && pqArray[idx] > pqArray[idx/2]) {
     swap(pqArray[idx], pqArray[idx/2]);
+    idx /=2;
+  }
+}
+
+void
+pqMax::sink(int idx) {
+  while(2*idx <= pqArray.size()-1){
+    int j = 2*idx;
+    if(j < pqArray.size()-1 && pqArray[j] < pqArray[j+1]) j++;
+    if(pqArray[idx] > pqArray[j]) break;
+    swap(pqArray[idx], pqArray[j]);
+    idx = j;
   }
 }
 
 int
-pqMax::getMax() { return pqArray[1]; }
+pqMax::readMax() { return pqArray[1]; }
+
+int
+pqMax::delMax() {
+  int max = pqArray[1];
+  swap(pqArray[1], pqArray[pqArray.size()-1]);
+  pqArray.pop_back();
+  sink(1);
+  return max;
+}
 
 int
 pqMax::count() { return pqArray.size()-1; }
 
+void
+pqMax::print() {
+  cout<<"left pq is :";
+  for (int i = 1; i < pqArray.size(); i++)
+    cout<< pqArray[i] <<' ';
+  cout<<endl;
+}
 
-
-
-
+bool
+pqMax::isEmpty() {
+  return pqArray.size() == 1;
+}
